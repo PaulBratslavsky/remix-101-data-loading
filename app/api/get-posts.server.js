bdb686116bba79d9f3139d5142986e38448c1ca9
paul bratslavsky <paulbratslavsky@pauls-Mac-mini.local>
2023-02-25 14:16:40 -0600

    complete

--- app/api/get-posts.server.js
+++ app/api/get-posts.server.js
@@ -1,34 +1,24 @@
-const data = [
-  {
-    title: 'My First Blog Post',
-    date: '2021-01-01',
-    image: 'https://images.unsplash.com/photo-1544717305-996b815c338c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80',
-    content: 'Hello world!',
-    slug: 'my-first-blog-post',
-  },
-  {
-    title: 'My Second Blog Post',
-    date: '2021-01-01',
-    image: 'https://images.unsplash.com/photo-1544717305-996b815c338c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80',
-    content: 'Hello Remix!',
-    slug: 'my-second-blog-post',
-  },
-  {
-    title: 'My Third Blog Post',
-    date: '2021-01-01',
-    image: 'https://images.unsplash.com/photo-1544717305-996b815c338c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80',
-    content: 'Hello Remix!',
-    slug: 'my-third-blog-post',
-  },
-  {
-    title: 'My Fourth Blog Post',
-    date: '2021-01-01',
-    image: 'https://images.unsplash.com/photo-1544717305-996b815c338c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80',
-    content: 'Hello Remix!',
-    slug: 'my-fourth-blog-post',
-  }
-]
+const qs = require('qs');
 
-export function getPosts() {
-  return data;
+const params = qs.stringify({
+  populate: {
+    image: {
+      populate: '*'
+    }
+  },
+});
+
+export async function getPosts() {
+  const baseUrl = 'http://localhost:1337'
+  const query = `/api/posts?${params}`
+
+  try {
+    const res = await fetch(baseUrl + query)
+    return await res.json()
+  }
+
+  catch (err) {
+    // TODO: Handle error
+    console.log(err)
+  }
 }
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -3,13 +3,25 @@ import { getPosts } from "~/api/get-posts.server.js"
 import { useLoaderData } from '@remix-run/react';
 import { json } from "@remix-run/node";
 
-export function loader() {
-  const data = getPosts();
+export async function loader() {
+  const data = await getPosts(); 
   return json(data);
 }
 
-export default function Index() {
-  const data = useLoaderData();
+function flattenData(data) {
+  return data.reduce((acc, curr) => {
+    const transformed = {
+      id: curr.id,
+      title: curr.attributes.title,
+      date: curr.attributes.publishedAt,
+      slug: curr.attributes.slug,
+      image: 'http://localhost:1337' + curr.attributes.image.data.attributes.url,
+    }
+    return [...acc, transformed]
+  }, [])
+}
 
-  return <BlogPostCardList data={data} />;
+export default function Index() {
+  const { data } = useLoaderData(); 
+  return <BlogPostCardList data={flattenData(data)} />;
 }
